2738dfe9c79466290c2d04bbd9237e61ec11c13c
Chris O'Brien <chrisobrien2000@outlook.com>
2020-06-05 16:30:36 -0500

    Added recipe book and shoppping list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component'
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './Shopping List/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './Shopping List/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './Recipe Book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './Recipe Book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './Recipe Book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
   ],
   imports: [
     BrowserModule,
