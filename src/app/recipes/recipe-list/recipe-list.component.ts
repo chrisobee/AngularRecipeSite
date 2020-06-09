fccbaaf9e7ecd7e026d03e38f2ed283f99587bcd
Chris O'Brien <chrisobrien2000@outlook.com>
2020-06-09 17:59:06 -0500

    Added recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Big Kahuna Burger',
+      'That IS a tasty burger!',
+      'https://upload.wikimedia.org/wikipedia/commons/e/eb/Pineapple_bacon_cheeseburger.jpg'
+    )
+  ];
   constructor() { }
 
   ngOnInit(): void {
