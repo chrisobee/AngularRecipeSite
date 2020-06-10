9196039b1b0f02eb1958a29cec814a9496c9783c
Chris O'Brien <chrisobrien2000@outlook.com>
2020-06-10 11:35:15 -0500

    Added ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+    // Use this typescript shortcut to create properties for the model and pass values to those props in the constructor 
+    constructor(public name: string, public amount: number){}
+}
